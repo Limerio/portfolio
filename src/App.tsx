aa1ed89366f59dab8d04ae5aad28daa573ba3f53
Limerio <44976231+Limerio@users.noreply.github.com>
2024-12-18 22:54:40 +0100

    chore: rewrite of the structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Section } from './components/ui/Section';
+
+export const App = () => (
+	<main className="bg-[#070c1a] text-[#d5d5d5] min-h-screen w-full flex justify-between items-center gap-3 p-40">
+		<Section>dddd</Section>
+		<Section>deeee</Section>
+	</main>
+);
---
+++ src/components/ui/Section.tsx
@@ -0,0 +1,5 @@
+import type { ReactNode } from 'react';
+
+export const Section = ({ children }: { children: ReactNode }) => (
+	<section className="">{children}</section>
+);
