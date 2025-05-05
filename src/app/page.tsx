3ff4ee8d9fb1a9b9b19b220aa22f671c99a12976
Limerio <44976231+Limerio@users.noreply.github.com>
2025-05-05 16:41:28 +0200

    chore(): remove project files and add redirect HTML page

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Title } from "@/components/Title";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col justify-center items-center p-24">
-      <Title />
-    </main>
-  );
-}
